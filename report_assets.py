aca1be8fe355033af990e72fe587e28a309135ac
Montalat Thibaut <montalat@Mac-mini-de-Montalat.local>
2026-06-24 15:02:19 +0200

    Rapport: logo via URL hébergée join-iad.com (s'affiche dans Gmail)

---
+++ report_assets.py
@@ -0,0 +1,10 @@
+"""
+Assets du rapport email : logo IAD et lien du tableau.
+"""
+
+GOOGLE_SHEET_URL = (
+    "https://docs.google.com/spreadsheets/d/1WYvelN50Hz_8gCo8o9BtsFpUUdLaxQbzlXAySSY4I9M"
+)
+
+# Logo IAD hébergé (s'affiche dans Gmail, contrairement à un data: URI base64)
+LOGO_DATA_URI = "https://www.join-iad.com/app/uploads/sites/9/2024/02/logo-iad-bleu.png"
